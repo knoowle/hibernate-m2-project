5ae84cb4e0f508a954b9ca27422c702025467d22
jim green <shunzheheliu@yeah.net>
2013-01-20 00:59:17 +0800

    xxx

--- hibernate-example/src/com/knoowle/util/HibernateUtil.java
+++ hibernate-example/src/com/knoowle/util/HibernateUtil.java
@@ -1,4 +1,13 @@
 package com.knoowle.util;
 
 public class HibernateUtil {
+	private static SessionFactory sessionFactory;
+
+	static {
+		sessionFactory = new Configurate().configure().buildSessionFactory();
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
 }
